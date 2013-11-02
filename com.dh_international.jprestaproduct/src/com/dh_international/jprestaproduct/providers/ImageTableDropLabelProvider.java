286b83b288820036dcff7cb80b8ae22c226f3eb7
Guillaume Sorbier <guillaume.sorbier@gmail.com>
2013-11-02 15:29:30 +0100

    Implementation correcte du Drad And Drop pour les images.

---
+++ com.dh_international.jprestaproduct/src/com/dh_international/jprestaproduct/providers/ImageTableDropLabelProvider.java
@@ -0,0 +1,4 @@
+U2FsdGVkX19x4o91wRhYy/lp/QK3+JBRlCVk/x28HiMwnBYgrk1sP9FoEcmEFLlj
+TOv20kUJsKtYizxGUtW9Qw6vVo++yPkMX9d9RSFyT32z9zaXmChDcr6Il/PX02no
+gl+YLex2j1REmrp2ordf5OD6wWBJZb5ZyC+w9rGEYmvJtyaOwSBK2SHozKwlMmK3
+p0QMgGoWw/LLe7hu1hNqwpB5or5VnzHyZ/vOazZc5jGb+vJURfGEJ9Vwbuw+vS1N
